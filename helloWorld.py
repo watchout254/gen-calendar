6bb1d05d114e6f0caf35247bb20b4771e8138c16
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2022-10-13 20:00:53 +0300

    ndio kuanzaa

---
+++ helloWorld.py
@@ -0,0 +1,25 @@
+
+#print("What's up")
+#print('You good??')
+#to comment we use the hashtag value 
+
+#red_bucket = input("What do you want to put in your bucket?")
+#print(red_bucket)
+
+Thomas_age = 10
+Age_at_kindergaten = 10
+
+if Thomas_age<Age_at_kindergaten:
+    print("Oooops!!Sorry you have not qualified")
+
+elif Thomas_age == Age_at_kindergaten:
+    print("Welcome mahn,, you are innit")
+
+else:
+    print("You should be in another class")
+
+import calendar 
+yy = 2022
+mm = 12
+
+print(calendar.month(yy,mm))
